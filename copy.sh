72c93974c7f9439870c15bdd8cd009b3dd53ab03
zpydee <benji@spawntech.io>
2024-04-10 13:28:14 +0200

    add copy scrip

---
+++ copy.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# DOTFILES=(.bash_profile .gitconfig .gitignore .zshrc)
+DOTFILES=(.zshrc)
+
+for dotfile in $(echo ${DOTFILES[*]});
+do
+    cp ~/dotfiles/$(echo $dotfile) ~/$(echo $dotfile)
+done
